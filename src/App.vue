ae809a47f78d1c02fb9f4befb88ff1eb367fe88b
LukaszTomal <tomal_l@poczta.onet.pl>
2022-05-28 14:45:12 +0200

    Next commit added additional components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    <h1> Twój email to {{email}}</h1>
-    <div v-if="email.length < 10">Ale masz krótki adres!</div>
-    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-    <div class="toLong"v-else>Twój adres e-mail jest stanowczo za długi.</div>
-    <input type="text" v-model="email" @keydown.enter="alertMyEmail()">
-    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+    <h1>Witaj w systemie do zapisów na zajęcie</h1>
+    <div v-if="!loggedIn">
+      <label>Zaloguj się mailem</label>
+      <input type="text" v-model="email">
+      <button @click="logMeIn">Zaloguj się</button>
+    </div>
+    <div v-else>
+      <h2> Witaj{{email}}</h2>
+      <a @click="logOut()">Wyloguj</a>
+    </div>
   </div>
 </template>
 
@@ -15,11 +19,16 @@ export default {
   data()  {
     return {
       email: '',
+      loggedIn: false
     };
   },
   methods: {
-    alertMyEmail() {
-      alert(this.email);
+    logMeIn(){
+      this.loggedIn = true;
+    },
+    logOut(){
+      this.loggedIn = false;
+      this.email = '';
     }
   }
 }
