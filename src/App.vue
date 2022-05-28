518fe0004a2860a45c89cbb7e1bd957a1a7f1a5a
LukaszTomal <tomal_l@poczta.onet.pl>
2022-05-28 13:58:17 +0200

    Second commit added additional components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1> Twój email to {{email}}</h1>
+    <div v-if="email.length < 10">Ale masz krótki adres!</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div class="toLong"v-else>Twój adres e-mail jest stanowczo za długi.</div>
     <input type="text" v-model="email">
   </div>
 </template>
@@ -18,5 +21,8 @@ export default {
 </script>
 
 <style>
+.toLong{
+  color: red;
+}
 
 </style>
